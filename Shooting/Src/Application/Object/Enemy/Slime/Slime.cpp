5ef1e2099399529039c96783244f8e4fc209fd69
kd1432756 <kd1432756@st.kobedenshi.ac.jp>
2026-04-24 15:02:30 +0900

    プレイヤーの状態追加

--- Shooting/Src/Application/Object/Enemy/Slime/Slime.cpp
+++ Shooting/Src/Application/Object/Enemy/Slime/Slime.cpp
@@ -2,6 +2,7 @@
 
 void Slime::Init()
 {
+	m_isActive = false;
 }
 
 void Slime::Update()
@@ -10,6 +11,8 @@ void Slime::Update()
 
 void Slime::Draw()
 {
+	SHADER.m_spriteShader.SetMatrix(Math::Matrix::CreateTranslation(m_pos.x, m_pos.y, 0));
+	SHADER.m_spriteShader.DrawTex(m_tex, Math::Rectangle{ 0, 0, 128, 128 }, 1.0f);
 }
 
 void Slime::Release()
